6e5e32c019db118c72cba8eeb65f6677180101d3
Mascari4615 <mascari4615@gmail.com>
2024-02-13 16:39:34 +0900

    MTarget

---
+++ MTarget/Example/MTarget_Example.cs
@@ -0,0 +1,25 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon.Common.Interfaces;
+
+namespace Mascari4615
+{
+	[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+	public class MTarget_Example : UdonSharpBehaviour
+	{
+		[SerializeField] private MTarget mTarget;
+		[SerializeField] private Transform teleportPos;
+
+		public override void Interact()
+		{
+			SendCustomNetworkEvent(NetworkEventTarget.All, nameof(TryTeleport));
+		}
+
+		public void TryTeleport()
+		{
+			if (mTarget.IsLocalPlayerTarget)
+				Networking.LocalPlayer.TeleportTo(teleportPos.position, teleportPos.rotation);
+		}
+	}
+}
---
+++ MTarget/Scripts/MTargetBool.cs
@@ -0,0 +1,26 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Mascari4615
+{
+	[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+	public class MTargetBool : CustomBool
+	{
+		[Header("_" + nameof(MTargetBool))]
+		[SerializeField] private MTarget _mTarget;
+		public MTarget MTarget => _mTarget;
+
+		protected override void Start()
+		{
+			OnValueChange();
+		}
+
+		public void UpdateValue()
+		{
+			SetValue(MTarget.IsLocalPlayerTarget);
+		}
+	}
+}
