cc1aa5e6aa275725282861a8f6f2dd3a3b5b79f2
Mascari4615 <mascari4615@gmail.com>
2024-04-04 17:39:28 +0900

    PosFixer

---
+++ PosFixer/PosFixerReset.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Mascari4615
+{
+    public class PosFixerReset : MBase
+    {
+        [SerializeField] private PosFixer[] posFixers;
+
+        public override void Interact()
+        {
+            foreach (var posFixer in posFixers)
+            {
+                SetOwner(posFixer.gameObject);
+                posFixer.transform.localPosition = posFixer.OriginPos;
+            }
+        }
+    }
+}
---
+++ PosFixer/TransformSync.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Mascari4615
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Continuous)]
+    public class TransformSync : MBase
+    {
+        [UdonSynced(UdonSyncMode.None)] private Vector3 syncPos;
+        [UdonSynced(UdonSyncMode.None)] private Quaternion syncRot;
+
+        private void Start()
+        {
+            UpdateTransform();
+        }
+
+        private void Update()
+        {
+            UpdateTransform();
+        }
+
+        private void UpdateTransform()
+        {
+            transform.position = syncPos;
+            transform.rotation = syncRot;
+        }
+
+        public void SetSyncPos(Vector3 newPos)
+        {
+            SetOwner();
+            syncPos = newPos;
+        }
+
+        private void SetSyncRot(Quaternion newRot)
+        {
+            SetOwner();
+            syncRot = newRot;
+        }
+    }
+}
