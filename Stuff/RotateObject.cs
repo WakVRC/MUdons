f630d314a74792643d9bfd38df96643268beebe6
Mascari4615 <mascari4615@gmail.com>
2024-04-08 15:54:14 +0900

    Stuff

---
+++ Stuff/RotateObject.cs
@@ -0,0 +1,17 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+public class RotateObject : UdonSharpBehaviour
+{
+    public float speed;
+
+    [SerializeField] private bool x;
+    [SerializeField] private bool y;
+    [SerializeField] private bool z;
+    [SerializeField] private Space space;
+
+    private void Update()
+    {
+        transform.Rotate(x ? Time.deltaTime * speed : 0, y ? Time.deltaTime * speed : 0, z ? Time.deltaTime * speed : 0, space);
+    }
+}
---
+++ Stuff/SpriteList.cs
@@ -0,0 +1,33 @@
+﻿using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Mascari4615
+{
+	// [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+	public class SpriteList : MBase
+	{
+		[Header("_" + nameof(SpriteList))]
+		[SerializeField] private MScore mScore;
+		[SerializeField] private Sprite[] spriteList;
+		[SerializeField] private Image targetImage;
+		[SerializeField] private SpriteRenderer targetSpriteRenderer;
+
+		public void UpdateSpriteByScore()
+		{
+			if (mScore)
+				UpdateSprite(mScore.Score);
+		}
+
+		public void UpdateSprite(int index)
+		{
+			if (targetImage)
+				for (var i = 0; i < spriteList.Length; i++)
+					targetImage.sprite = spriteList[index];
+
+			if (targetSpriteRenderer)
+				for (var i = 0; i < spriteList.Length; i++)
+					targetSpriteRenderer.sprite = spriteList[index];
+		}
+	}
+}
