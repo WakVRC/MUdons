66b0651bed45025fd977a82ea818e78084b3ed5c
Mascari4615 <mascari4615@gmail.com>
2024-05-09 17:13:13 +0900

    VoiceReseter 개선
    - 잘못된/어색한 표현 수정
    - VoiceAreaTag 기반 무시 기능 추가

--- MSound/Voice/VoiceSetter/VoiceReseter.cs
+++ MSound/Voice/VoiceSetter/VoiceReseter.cs
@@ -8,7 +8,10 @@ namespace Mascari4615
 	public class VoiceReseter : VoiceUpdater
 	{
 		[SerializeField] private VoiceManager voiceManager;
-		[SerializeField] private MTarget[] mTargets;
+		[SerializeField] private MTarget[] ignoreTargets;
+
+		[SerializeField] private bool useIngnoreTargetTag;
+		[SerializeField] private VoiceAreaTag IgnoreTargetTag;
 
 		public override void UpdateVoice()
 		{
@@ -18,14 +21,24 @@ namespace Mascari4615
 			if (voiceManager.PlayerApis == null)
 				return;
 
-			// 타겟이 아니라면 무시
-			foreach (MTarget mTarget in mTargets)
+			// 타겟이라면 무시
+			foreach (MTarget ignoreTarget in ignoreTargets)
 			{
-				if (mTarget.IsLocalPlayerTarget)
+				if (ignoreTarget.IsLocalPlayerTarget)
 					return;
 			}
 
-			// 타겟이라면 보이스 상태를 초기화
+			// 타겟 태그를 가지고 있으면 무시
+			if (useIngnoreTargetTag)
+			{
+				string tag =
+					Networking.LocalPlayer.GetPlayerTag($"{Networking.LocalPlayer.playerId}{IgnoreTargetTag}");
+
+				if (tag == TRUE_STRING)
+					return;
+			}
+
+			// 보이스 상태 초기화
 			for (int i = 0; i < voiceManager.PlayerApis.Length; i++)
 				voiceManager.VoiceStates[i] = VoiceState.Default;
 		}
