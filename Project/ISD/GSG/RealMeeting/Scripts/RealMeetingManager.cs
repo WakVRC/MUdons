b4e12bc82377bd08b3ce89970db1e0e6f84367ad
Mascari4615 <mascari4615@gmail.com>
2024-06-01 09:10:45 +0900

    세구님 리얼소개팅 컨텐츠 트리거 FIN
    - RespawnPickup -> RespawnObjectSync
    - WorldSpaceCanvas -> [Canvas] WorldSpace

--- Project/ISD/GSG/RealMeeting/Scripts/RealMeetingManager.cs
+++ Project/ISD/GSG/RealMeeting/Scripts/RealMeetingManager.cs
@@ -11,69 +11,65 @@ namespace Mascari4615.Project.ISD.GSG.RealMeeting
 	[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
 	public class RealMeetingManager : MBase
 	{
-		[SerializeField] private TextMeshProUGUI screenTitleText;
-		[SerializeField] private TextMeshProUGUI screenText;
+		[SerializeField] private Image screenImage;
+		[SerializeField] private Sprite[] screenSprites;
+		[SerializeField] private Sprite[] heartSprites;
 		[SerializeField] private string[] profileNames;
 
 		[SerializeField] private Transform mScoresParent;
 		private MScore[] mScores = new MScore[0];
 
 		[SerializeField] private CustomBool isPollTargetMale;
-		[SerializeField] private CustomBool isSecondPoll;
+
+		private UIHeartBlock[] heartBlocks;
 
 		private void Start()
 		{
-			mScores = new MScore[mScoresParent.childCount];
-
-			for (int i = 0; i < mScoresParent.childCount; i++)
-			{
-				Transform c = mScoresParent.GetChild(i);
-
-				TextMeshProUGUI nameText = c.GetChild(0).GetComponent<TextMeshProUGUI>();
-				nameText.text = profileNames[i];
-
-				mScores[i] = c.GetChild(1).GetComponent<MScore>();
-			}
-
-			MDebugLog($"Initialized {mScores.Length} Scores");
-
+			Init();
 			UpdateScreen();
 		}
 
-		public void SetScreen(bool targetMale, bool secondPoll)
+		public void Init()
 		{
-			if (secondPoll)
-				screenTitleText.text = $"{(targetMale ? "남성" : "여성")} 받은 하트 수";
-			else
-				screenTitleText.text = $"{(targetMale ? "남성" : "여성")} 첫인상 득표수";
-
-			// Not Initialized
-			if (mScores.Length == 0)
-				return;
-
-			string s = string.Empty;
-
-			// TODO: Heart Image
-			for (int i = 0; i < mScores.Length / 2; i++)
+			mScores = new MScore[mScoresParent.childCount];
+			for (int i = 0; i < mScoresParent.childCount; i++)
 			{
-				int actualIndex = targetMale ? i : (i + mScores.Length / 2);
+				Transform c = mScoresParent.GetChild(i).GetChild(0);
 
-				string name = profileNames[actualIndex];
-				s += $"{name}: "; 
-				
-				int score = mScores[actualIndex].Score;
-				for (int j = 0; j < score; j++)
-					s += "❤️";
+				TextMeshProUGUI debugNameText = c.GetChild(1).GetChild(5).GetComponent<TextMeshProUGUI>();
+				debugNameText.text = profileNames[i];
 
-				s += "\n";
+				mScores[i] = c.GetComponent<MScore>();
 			}
 
-			screenText.text = s;
+			heartBlocks = GetComponentsInChildren<UIHeartBlock>(true);
+			foreach (UIHeartBlock heartBlock in heartBlocks)
+				heartBlock.Init();
+
+			MDebugLog($"Initialized {mScores.Length} Scores");
 		}
 
 		public void UpdateScreen()
 		{
-			SetScreen(isPollTargetMale.Value, isSecondPoll.Value);
+			SetScreen(isPollTargetMale.Value);
+		}
+
+		public void SetScreen(bool targetMale)
+		{
+			// Not Initialized
+			if (mScores.Length == 0)
+				return;
+
+			screenImage.sprite = screenSprites[targetMale ? 0 : 1];
+			Sprite heartSprite = heartSprites[targetMale ? 0 : 1];
+
+			for (int i = 0; i < mScores.Length / 2; i++)
+			{
+				int actualIndex = targetMale ? i : (i + mScores.Length / 2);
+				int score = mScores[actualIndex].Score;
+
+				heartBlocks[i].UpdateUI(heartSprite, score);
+			}
 		}
 	}
 }
