fe3e834554c4d7a54aadc138092632489eb796ed
Mascari4615 <mascari4615@gmail.com>
2024-06-07 21:39:20 +0900

    MVideoPlayer

--- MUI/MFullscreen/MFullscreen.cs
+++ MUI/MFullscreen/MFullscreen.cs
@@ -1,60 +1,46 @@
 ﻿using UdonSharp;
-using UdonSharp.Video;
 using UnityEngine;
-using UnityEngine.UI;
-using VRC.SDK3.Video.Components.Base;
+using VRC.Udon.Common.Interfaces;
 
 namespace Mascari4615
 {
 	[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
 	public class MFullscreen : MBase
 	{
-		[SerializeField] private Renderer internalAvProRenderer;
-		[SerializeField] private BaseVRCVideoPlayer videoPlayer;
-
-		[SerializeField] private RawImage[] targetImages;
-
-		[SerializeField] private VideoScreenHandler videoScreenHandler;
-
-		[SerializeField] private CustomBool customBool;
-		[SerializeField] private bool hideVideoWhenIsNotPlaying = false;
-
+		[SerializeField] private CustomBool screenActive;
 		private CanvasGroup canvasGroup;
 
 		private void Start()
 		{
 			canvasGroup = GetComponent<CanvasGroup>();
 
-			if (!customBool)
-				SetScreenFalse();
+			if (!screenActive)
+				SetActiveFalse();
 		}
 
-		private void Update()
-		{
-			foreach (var targetImage in targetImages)
-				if (videoPlayer.IsPlaying) //Playing
-				{
-					targetImage.texture = videoScreenHandler.GetVideoTexture();
-					targetImage.color = Color.white;
-				}
-				else if (hideVideoWhenIsNotPlaying)
-				{
-					targetImage.texture = null;
-					targetImage.color = Color.white * 0;
-				}
-		}
+		public void SetActive(bool value) => canvasGroup.alpha = value ? 1 : 0;
 
-		public void ToggleScreen() => canvasGroup.alpha = canvasGroup.alpha == 0 ? 1 : 0;
-		public void SetScreen(bool value) => canvasGroup.alpha = value ? 1 : 0;
-		public void SetScreenTrue() => SetScreen(true);
-		public void SetScreenTrue_Global() => SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, nameof(SetScreenTrue));
-		public void SetScreenFalse() => SetScreen(false);
-		public void SetScreenFalse_Global() => SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, nameof(SetScreenFalse));
 
+		[ContextMenu(nameof(ToggleActive))]
+		public void ToggleActive() => SetActive(canvasGroup.alpha != 1);
+
+		[ContextMenu(nameof(SetActiveTrue))]
+		public void SetActiveTrue() => SetActive(true);
+
+		[ContextMenu(nameof(SetActiveTrue_Global))]
+		public void SetActiveTrue_Global() => SendCustomNetworkEvent(NetworkEventTarget.All, nameof(SetActiveTrue));
+
+		[ContextMenu(nameof(SetActiveFalse))]
+		public void SetActiveFalse() => SetActive(false);
+
+		[ContextMenu(nameof(SetActiveFalse_Global))]
+		public void SetActiveFalse_Global() => SendCustomNetworkEvent(NetworkEventTarget.All, nameof(SetActiveFalse));
+
+		[ContextMenu(nameof(UpdateValue))]
 		public void UpdateValue()
 		{
-			if (customBool)
-				SetScreen(customBool.Value);
+			if (screenActive)
+				SetActive(screenActive.Value);
 		}
 	}
 }
