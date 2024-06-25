3518449b4de9cf65b4aabe42636b2c84097c69c6
Mascari4615 <mascari4615@gmail.com>
2024-06-25 09:02:01 +0900

    MScore 디벨롭 외
    - ObjectActive로 대체 가능한 기존 MScore Canvas 기능 제외
    - UIMScore 분리
    
    - [Block] Base

---
+++ Packages/com.mascari4615.m-udons/Runtime/MScore/Scripts/Core/MScore.cs
@@ -0,0 +1,149 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+
+namespace Mascari4615
+{
+	[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+	public class MScore : MEventSender
+	{
+		[field:Header("_" + nameof(MScore))]
+		[field: SerializeField] public int MinScore{ get; private set; } = 0;
+		[field: SerializeField] public int MaxScore { get; private set; } = 1000;
+		[field: SerializeField] public int IncreaseAmount { get; private set; } = 1;
+		[field: SerializeField] public int DecreaseAmount { get; private set; }= 1;
+		[SerializeField] private int defaultScore = 0;
+		[SerializeField] private bool useClamp;
+		[SerializeField] private bool useLoop;
+		[SerializeField] private bool loopOption;
+
+		[SerializeField] private bool useSync;
+		[SerializeField] private CustomBool isMaxScore;
+		[SerializeField] private CustomBool isMinScore;
+
+		[SerializeField] private UIMScore[] uis;
+
+		[UdonSynced(), FieldChangeCallback(nameof(SyncedScore))] private int _syncedScore;
+		public int SyncedScore
+		{
+			get => _syncedScore;
+			set
+			{
+				_syncedScore = value;
+
+				if (useSync)
+					SetScore(_syncedScore, isReciever: true);
+			}
+		}
+		private int _score;
+		public int Score
+		{
+			get => _score;
+			set
+			{
+				_score = value;
+				OnScoreChange();
+			}
+		}
+
+		private void Start()
+		{
+			Init();
+		}
+
+		private void Init()
+		{
+			MDebugLog($"{nameof(Init)}");
+
+			foreach (UIMScore ui in uis)
+				ui.Init(this);
+
+			if (useSync)
+			{
+				if (Networking.IsMaster)
+				{
+					SyncedScore = defaultScore;
+					RequestSerialization();
+				}
+			}
+			else
+			{
+				SetScore(defaultScore);
+			}
+
+			OnScoreChange();
+		}
+
+		public void SetMinMaxScore(int min, int max)
+		{
+			MDebugLog($"{nameof(SetMinMaxScore)}");
+
+			MinScore = min;
+			MaxScore = max;
+
+			if (Score < MinScore || Score > MaxScore)
+				SetScore(Mathf.Clamp(Score, MinScore, MaxScore));
+		}
+
+		public void SetScore(int newScore, bool isReciever = false)
+		{
+			MDebugLog($"{nameof(SetScore)}");
+
+			int actualScore = newScore;
+
+			if (useClamp)
+			{
+				actualScore = Mathf.Clamp(actualScore, MinScore, MaxScore);
+			}
+			else if (useLoop)
+			{
+				if (actualScore > MaxScore)
+					actualScore = MinScore + (actualScore - MaxScore) + (loopOption ? -1 : 0);
+				else if (actualScore < MinScore)
+					actualScore = MaxScore - (MinScore - actualScore) + (loopOption ? 1 : 0);
+			}
+			else
+			{
+				if (actualScore > MaxScore)
+					return;
+
+				if (actualScore < MinScore)
+					return;
+			}
+
+			if (useSync)
+			{
+				if (SyncedScore != actualScore)
+				{
+					if (isReciever == false)
+					{
+						SetOwner();
+						SyncedScore = actualScore;
+						RequestSerialization();
+					}
+				}
+			}
+
+			Score = actualScore;
+		}
+
+		private void OnScoreChange()
+		{
+			MDebugLog(nameof(OnScoreChange));
+
+			if (isMaxScore != null)
+				isMaxScore.SetValue(Score == MaxScore);
+			
+			if (isMinScore != null)
+				isMinScore.SetValue(Score == MinScore);
+
+			SendEvents();
+		}
+
+		public void IncreaseScore() => SetScore(Score + IncreaseAmount);
+		public void AddScore(int amount) => SetScore(Score + amount);
+		public void DecreaseScore() => SetScore(Score - DecreaseAmount);
+		public void SubScore(int amount) => SetScore(Score - amount);
+		public void ResetScore() => SetScore(defaultScore);
+	}
+}
---
+++ Packages/com.mascari4615.m-udons/Runtime/MScore/Scripts/UI/UIMScore.cs
@@ -0,0 +1,51 @@
+﻿using TMPro;
+using UdonSharp;
+using UnityEngine;
+
+namespace Mascari4615
+{
+	[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+	public class UIMScore : MBase
+	{
+		[Header("_" + nameof(UIMScore))]
+		[SerializeField] private TextMeshProUGUI[] scoreTexts;
+		[SerializeField] private GameObject[] hardButtons;
+		[SerializeField] private bool printPlusOne;
+		private MScore mScore;
+
+		public void Init(MScore mScore)
+		{
+			this.mScore = mScore;
+			mScore.RegisterListener(this, nameof(UpdateUI));
+		}
+
+		public void UpdateUI()
+		{
+			int score = mScore.Score;
+			string scoreString = (score + (printPlusOne ? 1 : 0)).ToString();
+
+			foreach (TextMeshProUGUI scoreText in scoreTexts)
+				scoreText.text = scoreString;
+
+			for (int i = 0; i < hardButtons.Length; i++)
+				hardButtons[i].SetActive(mScore.MinScore <= i && i <= mScore.MaxScore);
+		}
+
+		public void IncreaseScore() => mScore.IncreaseScore();
+		public void AddScore10() => mScore.AddScore(mScore.IncreaseAmount * 10);
+		public void DecreaseScore() => mScore.DecreaseScore();
+		public void SubScore10() => mScore.SubScore(mScore.DecreaseAmount * 10);
+		public void ResetScore() => mScore.ResetScore();
+
+		public void SetScore0() => mScore.SetScore(0);
+		public void SetScore1() => mScore.SetScore(1);
+		public void SetScore2() => mScore.SetScore(2);
+		public void SetScore3() => mScore.SetScore(3);
+		public void SetScore4() => mScore.SetScore(4);
+		public void SetScore5() => mScore.SetScore(5);
+		public void SetScore6() => mScore.SetScore(6);
+		public void SetScore7() => mScore.SetScore(7);
+		public void SetScore8() => mScore.SetScore(8);
+		public void SetScore9() => mScore.SetScore(9);
+	}
+}
