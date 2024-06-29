9c698ff4e7e3396acbbb637860ff0e24bf6f39a0
Mascari4615 <mascari4615@gmail.com>
2024-06-29 22:25:45 +0900

    MAnimator HorribleEvents 외

--- Packages/com.mascari4615.m-udons/Runtime/MAnimation/MAnimator.cs
+++ Packages/com.mascari4615.m-udons/Runtime/MAnimation/MAnimator.cs
@@ -91,21 +91,49 @@ namespace Mascari4615
 		public void SetTrigger_L0() => SetTrigger_L(0);
 		public void SetTrigger_L1() => SetTrigger_L(1);
 		public void SetTrigger_L2() => SetTrigger_L(2);
+		public void SetTrigger_L3() => SetTrigger_L(3);
+		public void SetTrigger_L4() => SetTrigger_L(4);
+		public void SetTrigger_L5() => SetTrigger_L(5);
+		public void SetTrigger_L6() => SetTrigger_L(6);
+		public void SetTrigger_L7() => SetTrigger_L(7);
+		public void SetTrigger_L8() => SetTrigger_L(8);
+		public void SetTrigger_L9() => SetTrigger_L(9);
 
 		[ContextMenu(nameof(SetTrigger_G0))]
 		public void SetTrigger_G0() => SetTrigger_G(0);
 		public void SetTrigger_G1() => SetTrigger_G(1);
 		public void SetTrigger_G2() => SetTrigger_G(2);
+		public void SetTrigger_G3() => SetTrigger_G(3);
+		public void SetTrigger_G4() => SetTrigger_G(4);
+		public void SetTrigger_G5() => SetTrigger_G(5);
+		public void SetTrigger_G6() => SetTrigger_G(6);
+		public void SetTrigger_G7() => SetTrigger_G(7);
+		public void SetTrigger_G8() => SetTrigger_G(8);
+		public void SetTrigger_G9() => SetTrigger_G(9);
 
 		[ContextMenu(nameof(SetInt_L0))]
 		public void SetInt_L0() => SetInt_L(0);
 		public void SetInt_L1() => SetInt_L(1);
 		public void SetInt_L2() => SetInt_L(2);
+		public void SetInt_L3() => SetInt_L(3);
+		public void SetInt_L4() => SetInt_L(4);
+		public void SetInt_L5() => SetInt_L(5);
+		public void SetInt_L6() => SetInt_L(6);
+		public void SetInt_L7() => SetInt_L(7);
+		public void SetInt_L8() => SetInt_L(8);
+		public void SetInt_L9() => SetInt_L(9);
 
 		[ContextMenu(nameof(SetInt_G0))]
 		public void SetInt_G0() => SetInt_G(0);
 		public void SetInt_G1() => SetInt_G(1);
 		public void SetInt_G2() => SetInt_G(2);
+		public void SetInt_G3() => SetInt_G(3);
+		public void SetInt_G4() => SetInt_G(4);
+		public void SetInt_G5() => SetInt_G(5);
+		public void SetInt_G6() => SetInt_G(6);
+		public void SetInt_G7() => SetInt_G(7);
+		public void SetInt_G8() => SetInt_G(8);
+		public void SetInt_G9() => SetInt_G(9);
 		#endregion
 	}
 }
