76059cd255d391a5d074955b2b8e9ffdbefe38b7
Mascari4615 <mascari4615@gmail.com>
2024-10-08 15:21:43 +0900

    CustomValueUISetter 디벨롭
    - MValue, MTarget 타입 대응
    - 경로 변경

---
+++ Packages/com.wakvrc.m-udons/Runtime/CustomValue/MTarget/Scripts/UI/UIMTargetSetter.cs
@@ -0,0 +1,37 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+namespace WakVRC
+{
+	[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+	public class UIMTargetSetter : MBase
+	{
+		[Header("_" + nameof(UIMTargetSetter))]
+		[SerializeField] private UIMTarget[] mTargetUIs;
+		[SerializeField] private MTarget[] mTargets;
+		[SerializeField] private MValue mTargetIndex;
+
+		private void Start()
+		{
+			Init();
+		}
+
+		private void Init()
+		{
+			mTargetIndex.SetMinMaxValue(0, mTargets.Length - 1);
+			mTargetIndex.RegisterListener(this, nameof(UpdateUI));
+			UpdateUI();
+		}
+
+		public void UpdateUI()
+		{
+			MDebugLog($"{nameof(UpdateUI)} - {mTargetIndex.Value}");
+
+			int index = mTargetIndex.Value;
+			MTarget mTarget = mTargets[index];
+
+			foreach (UIMTarget mTargetUI in mTargetUIs)
+				mTargetUI.SetMTarget(mTarget);
+		}
+	}
+}
---
+++ Packages/com.wakvrc.m-udons/Runtime/CustomValue/MValue/Scripts/UI/UIMValueSetter.cs
@@ -0,0 +1,37 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+namespace WakVRC
+{
+	[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+	public class UIMValueSetter : MBase
+	{
+		[Header("_" + nameof(UIMTargetSetter))]
+		[SerializeField] private UIMValue[] mValueUIs;
+		[SerializeField] private MValue[] mValues;
+		[SerializeField] private MValue mValueIndex;
+
+		private void Start()
+		{
+			Init();
+		}
+
+		private void Init()
+		{
+			mValueIndex.SetMinMaxValue(0, mValues.Length - 1);
+			mValueIndex.RegisterListener(this, nameof(UpdateUI));
+			UpdateUI();
+		}
+
+		public void UpdateUI()
+		{
+			MDebugLog($"{nameof(UpdateUI)} - {mValueIndex.Value}");
+
+			int index = mValueIndex.Value;
+			MValue mValue = mValues[index];
+
+			foreach (UIMValue mValueUI in mValueUIs)
+				mValueUI.SetMValue(mValue);
+		}
+	}
+}
