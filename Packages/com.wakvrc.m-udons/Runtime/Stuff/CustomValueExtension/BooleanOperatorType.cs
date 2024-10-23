4dc2c07a6717149539501d6e7687dac99f026d7d
카모뜨린・Mascari4615 <mascari4615@gmail.com>
2024-10-24 00:08:31 +0900

    MBooleanOperator, MValueComparisonOperator
    - 지엽적인 두 연산 우동 추가

---
+++ Packages/com.wakvrc.m-udons/Runtime/Stuff/CustomValueExtension/BooleanOperatorType.cs
@@ -0,0 +1,8 @@
+namespace Mascari4615
+{
+	public enum BooleanOperatorType
+	{
+		AND,
+		OR,
+	}
+}
---
+++ Packages/com.wakvrc.m-udons/Runtime/Stuff/CustomValueExtension/ComparisonOperatorType.cs
@@ -0,0 +1,12 @@
+namespace Mascari4615
+{
+	public enum ComparisonOperatorType
+	{
+		EQUAL,
+		NOT_EQUAL,
+		GREATER_THAN,
+		GREATER_THAN_OR_EQUAL,
+		LESS_THAN,
+		LESS_THAN_OR_EQUAL,
+	}
+}
